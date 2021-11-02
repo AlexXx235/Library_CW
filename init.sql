ba6099ac5881319a33cb0628e275b91bc7c33357
Danila <AlexXxKNY@mail.ru>
2021-11-03 00:52:12 +0300

    database redesigned, queries changed

--- init.sql
+++ init.sql
@@ -1,15 +1,20 @@
+-- Create library database
+CREATE DATABASE IF NOT EXISTS library;
+USE library;
+
 -- Create rooms (reading room) table
 CREATE TABLE IF NOT EXISTS rooms (
-    name VARCHAR(255) PRIMARY KEY,
-    capacity INT NOT NULL -- Each reading room have a limit of readers
+    name VARCHAR(128) PRIMARY KEY,
+    capacity INT UNSIGNED NOT NULL -- Each reading room have a limit of readers
 );
 
 -- Create readers table
 CREATE TABLE IF NOT EXISTS readers (
     card_number INT UNSIGNED PRIMARY KEY, -- Not bank card, but reading card
-    surname VARCHAR(255) NOT NULL,
+    name VARCHAR(128) NOT NULL,
+    surname VARCHAR(128) NOT NULL,
     phone_number VARCHAR(11) NOT NULL UNIQUE,
-    room_name VARCHAR(255),
+    room_name VARCHAR(128) NOT NULL,
     registration_date DATE NOT NULL,
     -- Each reader is placed in one of the rooms
     CONSTRAINT fk_reader_room_name FOREIGN KEY (room_name) REFERENCES rooms (name)
@@ -18,19 +23,26 @@ CREATE TABLE IF NOT EXISTS readers (
 -- Create books table
 CREATE TABLE IF NOT EXISTS books (
     cipher INT UNSIGNED PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    publish_year YEAR NOT NULL,
-    room_name VARCHAR(255) NOT NULL,
-    -- Each book is contained in some reading room
-    CONSTRAINT fk_book_room_name FOREIGN KEY (room_name) REFERENCES rooms (name)
+    title VARCHAR(256) NOT NULL,
+    author VARCHAR(256) NOT NULL
+);
+
+-- Create book_copies table
+CREATE TABLE IF NOT EXISTS book_copies (
+    inv_number INT UNSIGNED PRIMARY KEY,
+    cipher INT UNSIGNED NOT NULL,
+    room_name VARCHAR(128) NOT NULL,
+    release_year YEAR NOT NULL,
+
+    CONSTRAINT fk_copy_cipher FOREIGN KEY (cipher) REFERENCES books (cipher) ON UPDATE CASCADE,
+    CONSTRAINT fk_copy_room_name FOREIGN KEY (room_name) REFERENCES rooms (name)
 );
 
 -- Create taking_log table which contains records about taking books
 CREATE TABLE IF NOT EXISTS log (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     reader_card_number INT UNSIGNED NOT NULL, -- who took
-    book_cipher INT UNSIGNED NOT NULL, -- what took
+    book_copy_inv_number INT UNSIGNED NOT NULL, -- what took
     taking_date DATE NOT NULL, -- when took
     returning_date DATE, -- when returned
     -- References to reader and book
@@ -38,7 +50,6 @@ CREATE TABLE IF NOT EXISTS log (
         FOREIGN KEY (reader_card_number)
             REFERENCES readers (card_number),
     CONSTRAINT fk_book_cipher
-        FOREIGN KEY (book_cipher)
-            REFERENCES books (cipher)
-                ON UPDATE CASCADE -- cipher can be updated
+        FOREIGN KEY (book_copy_inv_number)
+            REFERENCES book_copies (inv_number)
 );
--- library_queries.py
+++ library_queries.py
@@ -1,38 +1,38 @@
 any_room_text = 'Любой читальный зал'
 
 
-def get_books_by_reader(cursor, card_number):
+def start_transaction(cursor):
+    cursor.execute('START TRANSACTION;')
+
+
+def commit(cursor):
+    cursor.execute('COMMIT;')
+
+
+def add_book(cursor, cipher, title, author):
     query = '''
-        SELECT b.cipher, b.title, b.author, l.taking_date, b.room_name
-        FROM books b, log l
-        WHERE b.cipher=l.book_cipher
-        AND l.returning_date IS NULL
-        AND l.reader_card_number=%s;
+        INSERT INTO books
+        VALUES (%s, %s, %s);
     '''
-    cursor.execute(query, (card_number, ))
-    return [book for book in cursor]
+    cursor.execute(query, (cipher, title, author))
 
 
-def get_books_by_author(cursor, author):
+def add_book_copy(cursor, inv_number, cipher, room_name, release_year):
     query = '''
-        SELECT *
-        FROM books
-        WHERE author=%s;
+        INSERT INTO book_copies
+        VALUES (%s, %s, %s, %s);
     '''
-    cursor.execute(query, (author, ))
-    books = [book for book in cursor]
-    return books
+    cursor.execute(query, (inv_number, cipher, room_name, release_year))
 
 
-def get_books_by_title(cursor, title):
+def get_count_of_book_copies(cursor, cipher):
     query = '''
-            SELECT *
-            FROM books
-            WHERE title=%s;
-        '''
-    cursor.execute(query, (title,))
-    books = [book for book in cursor]
-    return books
+        SELECT count(*)
+        FROM book_copies
+        WHERE cipher=%s;
+    '''
+    cursor.execute(query, (cipher, ))
+    return cursor.fetchone()[0]
 
 
 def delete_book(cursor, cipher):
@@ -44,6 +44,28 @@ def delete_book(cursor, cipher):
     cursor.execute(query, (cipher, ))
 
 
+def get_reader_holding_book_copy(cursor, inv_number):
+    query = '''
+        SELECT *
+        FROM readers
+        WHERE card_number IN(
+            SELECT reader_card_number 
+            FROM log
+            WHERE book_copy_inv_number=%s);
+    '''
+    cursor.execute(query, (inv_number, ))
+    return cursor.fetchone()
+
+
+def delete_book_copy(cursor, inv_number):
+    query = '''
+        DELETE 
+        FROM book_copies
+        WHERE inv_number=%s;
+    '''
+    cursor.execute(query, (inv_number, ))
+
+
 def change_book_cipher(cursor, old_cipher, new_cipher):
     query = '''
         UPDATE books
@@ -53,29 +75,115 @@ def change_book_cipher(cursor, old_cipher, new_cipher):
     cursor.execute(query, (new_cipher, old_cipher))
 
 
+def get_empty_slots_count_by_room(cursor, room_name):
+    query = '''
+        SELECT (rooms.capacity - count(*))
+        FROM rooms, readers
+        WHERE rooms.name=%s
+        AND readers.room_name=%s
+        GROUP BY rooms.name; 
+    '''
+    cursor.execute(query, (room_name, room_name))
+    return cursor.fetchone()[0]
+
+
+def add_reader(cursor, reader):
+    card_number, name, surname, phone_number, room_name = reader
+    query = '''
+        INSERT INTO readers
+        VALUES(%s, %s, %s, %s, %s);
+    '''
+    cursor.execute(query, (card_number,name, surname, phone_number, room_name))
+
+
+def get_books_by_reader(cursor, card_number):
+    query = '''
+        WITH numbers AS
+            (SELECT book_copy_inv_number AS num
+             FROM log 
+             WHERE reader_card_number = %s
+             AND returning_date IS NULL),
+             pairs AS
+            (SELECT bc.cipher, bc.inv_number
+             FROM book_copies bc, numbers
+             WHERE bc.inv_number = numbers.num)
+        SELECT b.*, bc.inv_number, bc.room_name, bc.release_year
+        FROM books b, book_copies bc, pairs
+        WHERE b.cipher = pairs.cipher
+        AND bc.inv_number = pairs.inv_number;
+    '''
+    cursor.execute(query, (card_number, ))
+    return cursor.fecthall()
+
+
+def delete_reader(cursor, card_number):
+    query = '''
+        DELETE FROM readers
+        WHERE card_number = %s;
+    '''
+    cursor.execute(query, (card_number, ))
+
+
+def book_loaning(cursor, card_number, inv_number):
+    query = '''
+        INSERT INTO log (reader_card_number, book_copy_inv_number, taking_date, returning_date)
+        VALUES (%s, %s, CURDATE(), NULL);
+    '''
+    cursor.execute(query, (card_number, inv_number))
+
+
+def book_returning(cursor, inv_number):
+    query = '''
+        UPDATE log
+        SET returning_date = CURDATE()
+        WHERE book_copy_inv_number = %s
+        AND returning_date IS NULL;
+    '''
+    cursor.execute(query, (inv_number, ))
+
+
 def get_book_by_cipher(cursor, cipher):
     query = '''
         SELECT * FROM books WHERE cipher=%s; 
     '''
     cursor.execute(query, (cipher, ))
-    return [book for book in cursor]
+    return cursor.fetchone()
 
 
-def filtered_book_search(cursor, title='', author='', room=''):
+def filtered_book_search(cursor, title='', author=''):
     title = '%' + title + '%'
     author = '%' + author + '%'
-    if room == any_room_text:
-        room = '%'
     query = '''
         SELECT *
         FROM books
         WHERE title LIKE %s
-        AND author LIKE %s
-        AND room_name LIKE %s
-        ORDER BY room_name;
+        AND author LIKE %s;
     '''
-    cursor.execute(query, (title, author, room))
-    return [book for book in cursor]
+    cursor.execute(query, (title, author))
+    return cursor.fetchall()
+
+
+def book_copy_search_by_inv_number(cursor, inv_number):
+    query = '''
+        SELECT *
+        FROM book_copies
+        WHERE inv_number = %s;
+    '''
+    cursor.execute(query, (inv_number, ))
+    return cursor.fetchone()
+
+
+def filtered_book_copy_search(cursor, cipher='%', year_from=0, year_till=-1, room_name='%'):
+    if year_till == -1:
+        year_till = 'YEAR(CURDATE())'
+    query = '''
+        SELECT *
+        FROM book_copies
+        WHERE cipher LIKE %s
+        AND release_year BETWEEN %s AND %s
+        AND room_name LIKE %s;
+    '''
+    cursor.execute(query, (cipher, year_from, year_till, room_name))
 
 
 def get_rooms(cursor):
@@ -86,57 +194,14 @@ def get_rooms(cursor):
     return [row[0] for row in cursor]
 
 
-def get_reader_by_book(cursor, cipher):
-    query = '''
-        SELECT *
-        FROM readers
-        WHERE card_number 
-        IN (SELECT reader_card_number
-            FROM log
-            WHERE book_cipher=%s
-            AND returning_date IS NULL);
-    '''
-    cursor.execute(query, (cipher, ))
-    return [reader for reader in cursor]
-
-
 def get_reader_by_phone_number(cursor, phone_number):
     query = '''
         SELECT *
         FROM readers
-        WHERE phone_number=%s;
+        WHERE phone_number = %s;
     '''
     cursor.execute(query, (phone_number, ))
-    return [reader for reader in cursor]
-
-
-def add_reader(cursor, reader):
-    surname, phone_number, card_number, room_name = reader
-    if if_room_have_place(cursor, room_name):
-        query = '''
-            INSERT INTO readers (card_number, surname, phone_number, room_name, registration_date)
-            VALUES (%s, %s, %s, %s, current_date());
-        '''
-        cursor.execute(query, (card_number, surname, phone_number, room_name))
-
-
-def get_room_capacities(cursor):
-    query = '''
-        SELECT capacity
-        FROM rooms;
-    '''
-    cursor.execute(query)
-    return [capacity for capacity in cursor]
-
-
-def get_room_sizes(cursor):
-    query = '''
-        SELECT room_name, count(*) AS size
-        FROM readers
-        GROUP BY room_name;
-    '''
-    cursor.execute(query)
-    return [room for room in cursor]
+    return cursor.fetchall()
 
 
 def get_rooms_table(cursor):
@@ -155,28 +220,7 @@ def get_rooms_table(cursor):
         ORDER BY name;
     '''
     cursor.execute(query)
-    return [row for row in cursor]
-
-
-def if_room_have_place(cursor, room):
-    cap_query = '''
-        SELECT capacity
-        FROM rooms
-        WHERE name=%s;
-    '''
-    size_query = '''
-        SELECT count(*)
-        FROM readers
-        GROUP BY room_name
-        HAVING room_name=%s;
-    '''
-    cursor.execute(cap_query, (room, ))
-    result = [row for row in cursor]
-    capacity = result[0][0]
-    cursor.execute(size_query, (room, ))
-    result = [row for row in cursor]
-    size = result[0][0]
-    return size < capacity
+    return cursor.fetchall()
 
 
 def current_readers_count(cursor):
@@ -212,13 +256,17 @@ def new_readers_for_month_count(cursor, year, month):
 
 def books_taken_for_month(cursor, year, month):
     query = '''
-      SELECT books.*, count(*)
-      FROM log, books
-      WHERE log.taking_date
-      BETWEEN '%(year)s-%(month)s-01'
-      AND LAST_DAY(DATE('%(year)s-%(month)s-01'))
-      AND log.book_cipher=books.cipher
-      GROUP BY book_cipher;  
+        WITH numbers AS 
+            (SELECT book_copy_inv_number AS num
+             FROM log where taking_date BETWEEN '%(year)s-%(month)s-01' AND LAST_DAY('%(year)s-%(month)s-01'),
+             ciphers AS
+            (SELECT cipher, count(*) as count
+             FROM book_copies, numbers
+             WHERE inv_number = numbers.num
+             GROUP BY cipher)
+        SELECT b.cipher, b.title, b.author, ciphers.count
+        FROM books b, ciphers
+        WHERE b.cipher = ciphers.cipher;
     '''
     cursor.execute(query, {'year': year, 'month': month})
     return [book for book in cursor]
@@ -226,7 +274,7 @@ def books_taken_for_month(cursor, year, month):
 
 def inactive_readers_for_month(cursor, year, month):
     query = '''
-        SELECT card_number, surname, phone_number, room_name
+        SELECT *
         FROM readers
         WHERE card_number NOT IN(
             SELECT reader_card_number
@@ -237,7 +285,7 @@ def inactive_readers_for_month(cursor, year, month):
         AND registration_date <= LAST_DAY(DATE('%(year)s-%(month)s-01'));
     '''
     cursor.execute(query, {'year': year, 'month': month})
-    return [reader for reader in cursor]
+    return cursor.fetchall()
 
 
 def get_current_year_and_month(cursor):
